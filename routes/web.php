57d7c16d4bb2d610951597cd66b0519e9b99cfcd
FebriIT <bisnisfebrii@gmail.com>
2024-09-01 22:49:28 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('front.master');
+});
+Route::get('login',[AuthController::class,'login'])->name('login');
+Route::post('/proses_login', [AuthController::class, 'proses_login'])->name('proses_login');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+Route::prefix('admin')->middleware('auth', 'role:admin')->name('admin_')->group(function () {
+
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/item', [ItemController::class, 'index'])->name('item');
+    Route::post('/item/create', [ItemController::class, 'create'])->name('itemcreate');
+});
